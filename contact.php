998beb75c5d78a5863cf17d3ec9fb96982f1ca51
rstonyg <109114092+rstonyg@users.noreply.github.com>
2022-07-11 16:11:31 -0700

    Update contact.php

--- contact.php
+++ contact.php
@@ -10,7 +10,7 @@
 <?php include("includes/navigation.php");?>
 
 <div class="container" id="main-content">
-	<h2>Contact Us</h2>
+	<h2>Contact Us or Them</h2>
 	<p>"Contact Us" conten goes here.  I'll stick with teh "lorem ipsum" as well, so that the footer isn't immediately following this text.</p>
 
 	<p>
